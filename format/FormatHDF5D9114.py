3f1c698f3865ce0a0a4f16f213abeac5e5875ff2
Derek Mendez <dermen@lbl.gov>
2018-12-21 00:59:21 -0800

    format hdf5 class added

---
+++ format/FormatHDF5D9114.py
@@ -0,0 +1,169 @@
+from __future__ import absolute_import, division
+
+import h5py
+import numpy as np
+
+from dxtbx.format.Format import Format
+from dxtbx.format.FormatHDF5 import FormatHDF5
+from dxtbx.format.FormatStill import FormatStill
+from scitbx import matrix
+from dials.array_family import flex
+from cxid9114.index.ddi import WAVELEN_LOW
+from cxid9114.common_mode.pppg import pppg
+
+# required HDF5 keys
+REQUIRED_KEYS = ['gain_val',
+                 'panel_gainmasks',
+                 'panel_masks',
+                 'panel_x',
+                 'panel_y',
+                 'panel_z',
+                 'panels',
+                 'pedestal']
+
+PIXEL_SIZE = 0.10992  # CSPAD pixel size in mm
+IMG_SIZE = (1800, 1800)
+X_OFFSET = 0
+Y_OFFSET = 0
+BITMAX = 2 ** 14
+PPPG_ARGS = {'Nhigh': 100.0,
+             'Nlow': 100.0,
+             'high_x1': -5.0,
+             'high_x2': 5.0,
+             'inplace': True,
+             'low_x1': -5.0,
+             'low_x2': 5.0,
+             'plot_details': False,
+             'plot_metric': False,
+             'polyorder': 3,
+             'verbose': False,
+             'window_length': 51}
+
+
+class FormatHDF5D9114(FormatHDF5, FormatStill):
+    """
+    Class for reading D9114 HDF5 hit files
+    script (this script lives on the SACLA hpc).
+    """
+
+    @staticmethod
+    def understand(image_file):
+        h5_handle = h5py.File(image_file, 'r')
+        h5_keys = h5_handle.keys()
+        understood = all([k in h5_keys for k in REQUIRED_KEYS])
+        return understood
+
+    def __init__(self, image_file, **kwargs):
+        from dxtbx import IncorrectFormatError
+        if not self.understand(image_file):
+            raise IncorrectFormatError(self, image_file)
+        FormatHDF5.__init__(self, image_file, **kwargs)
+
+        #def _start(self):
+        self._h5_handle = h5py.File(self.get_image_file(), 'r')
+        self.load_dark()
+        self.load_gain()
+        self.load_mask()
+        self.load_xyz()
+        self._geometry_define()
+        self._assembler_define()
+
+    def load_dark(self):
+        self.dark = self._h5_handle["pedestal"].value
+        assert (self.dark.dtype == np.float64)
+
+    def load_gain(self):
+        self.gain = self._h5_handle["panel_gainmasks"].value
+        assert (self.gain.dtype == np.bool)
+        self.gain_val = self._h5_handle["gain_val"].value
+
+    def load_mask(self):
+        self.mask = self._h5_handle["panel_masks"].value
+        assert (self.mask.dtype == np.bool)
+
+    def load_xyz(self):
+        self.panel_X = self._h5_handle["panel_x"].value
+        self.panel_Y = self._h5_handle["panel_y"].value
+        self.panel_Z = self._h5_handle["panel_z"].value
+
+    def _assembler_define(self):
+        bins0 = np.arange(-IMG_SIZE[0]/2, IMG_SIZE[0]/2+1)
+        bins1 = np.arange(-IMG_SIZE[1]/2, IMG_SIZE[1]/2+1)
+        assert(len(bins0) == IMG_SIZE[0]+1)
+        assert(len(bins1) == IMG_SIZE[1]+1)
+        self.hist_args = {"x": self.panel_X.ravel(),
+                          "y": self.panel_Y.ravel(),
+                          "bins": [bins0, bins1]}
+
+    def _geometry_define(self):
+        orig_x = -IMG_SIZE[0]*PIXEL_SIZE*.5 + X_OFFSET
+        orig_y = IMG_SIZE[1]*PIXEL_SIZE*.5 + X_OFFSET
+        orig_z = self.panel_Z.mean()
+        orig = (orig_x, orig_y, orig_z)
+
+        fast_scan = matrix.col((1.0, 0.0, 0.0))
+        slow_scan = matrix.col((0.0, -1.0, 0.0))
+
+        trusted_range = (-200, 2**14)
+        name = ""
+        self._cctbx_detector = \
+            self._detector_factory.make_detector(
+                name,
+                fast_scan,
+                slow_scan,
+                orig,
+                (PIXEL_SIZE, PIXEL_SIZE),
+                IMG_SIZE,
+                trusted_range)
+
+        self._cctbx_beam = self._beam_factory.simple(WAVELEN_LOW)
+
+    # def _detector(self, index=None):
+    #    return self._cctbx_detector
+
+    # def _beam(self, index=None):
+    #    return self._cctbx_beam
+
+    def get_num_images(self):
+        return self._h5_handle["panels"].shape[0]
+
+    def _assemble_panels(self):
+        self.panel_img = np.histogram2d(
+            x=self.hist_args["x"],
+            y=self.hist_args["y"],
+            bins=self.hist_args["bins"],
+            weights=self.panels.ravel())[0]
+        self.panel_img = np.ascontiguousarray(self.panel_img)
+
+    def get_raw_data(self, index=0):
+        self.panels = self._h5_handle['panels'][index].astype(np.float64)
+        self._correct_panels()
+        self._assemble_panels()
+        return flex.double(self.panel_img)
+
+    def _correct_panels(self):
+        self.panels = self.panels.astype(np.float64)
+        self.panels -= self.dark
+        pppg(self.panels,
+             self.gain,
+             self.mask,
+             **PPPG_ARGS)
+        self.panels[self.gain] = self.panels[self.gain]*self.gain_val
+
+    def get_detectorbase(self, index=None):
+        raise NotImplementedError
+
+    def get_image_file(self, index=None):
+        return Format.get_image_file(self)
+
+    def get_detector(self, index=None):
+        return self._cctbx_detector
+
+    def get_beam(self, index=None):
+        return self._cctbx_beam
+
+if __name__ == '__main__':
+    import sys
+
+    for arg in sys.argv[1:]:
+        print(FormatHDF5D9114.understand(arg))
--- format/FormatXTCD9114.py
+++ format/FormatXTCD9114.py
@@ -2,15 +2,22 @@ from __future__ import absolute_import, division, print_function
 
 import numpy as np
 import psana
-
+try:
+    import pylab as plt
+    CAN_PLOT = True
+except ImportError:
+    CAN_PLOT=False
 from xfel.cxi.cspad_ana.cspad_tbx import env_distance
 from cxid9114.common_mode.pppg import pppg
 from cxid9114.mask import mask_utils
-from cxid9114 import assemble_cspad
-from scitbx import matrix
-from dxtbx.model.detector import DetectorFactory
+from cxid9114.index.ddi import WAVELEN_LOW
+# from cxid9114 import assemble_cspad
+# from scitbx import matrix
+# from dxtbx.model.detector import DetectorFactory
+from dxtbx.model.beam import BeamFactory
 from dxtbx.format.FormatXTC import locator_str
 from dxtbx.format.FormatXTCCspad import FormatXTCCspad, cspad_locator_str
+from dxtbx.format.FormatXTC import FormatXTC
 from scitbx.array_family import flex
 from libtbx import phil
 
@@ -44,15 +51,14 @@ MASK2 = mask_utils.load_mask("corners_mask")
 MASK3 = mask_utils.load_mask("small_regions_mask")
 CSPAD_MASK = MASK1*MASK2*MASK3
 
-
-class FormatD9114(FormatXTCCspad):
+class FormatXTCD9114(FormatXTCCspad):
     run_number = None  # type: int
 
     def __init__(self, image_file, **kwargs):
         assert (self.understand(image_file))
         FormatXTCCspad.__init__(self, image_file, locator_scope=d9114_locator_scope, **kwargs)
 
-        self._ds = FormatXTCCspad._get_datasource(image_file, self.params)
+        self._ds = FormatXTC._get_datasource(image_file, self.params)
         self.run_number = self.params.run[0]
         self.cspad = psana.Detector(self.params.detector_address[0])
         self.dark = self.cspad.pedestals(self.run_number).astype(np.float64)
@@ -64,7 +70,7 @@ class FormatD9114(FormatXTCCspad):
         self.nominal_gain_val = self.cspad._gain_mask_factor
         self.populate_events()
         self.n_images = len(self.times)
-        self.params = FormatD9114.get_params(image_file)
+        self.params = FormatXTCD9114.get_params(image_file)
         self._set_pppg_args()
         self._set_psf()
         self._set_2d_img_info()
@@ -72,15 +78,32 @@ class FormatD9114(FormatXTCCspad):
                                               self._ds.env(), self.params.cspad.detz_offset)
         # self.feespec = psana.Detector("FeeSpec-bin")
 
+    def get_num_images(self):
+        return len(self.times)
+
     def _set_2d_img_info(self):
         dummie_event = self._get_event(0)
         self.img2d_mask = self.cspad.image(dummie_event, self.cspad_mask )\
             .astype(int).astype(bool)
         self.img_sh = self.img2d_mask.shape
 
-    def show_data(self, index):
+    def show_data(self, index, **kwargs):
+        """
+        plots the 2d image and returns the image axis and figure
+        :param index:
+        :return:
+        """
         data = self.get_psana_data(index)* self.cspad_mask
-        assemble_cspad.assemble_cspad(data, self.psf)
+        self.img2d = self.cspad.image( self.event, data)
+        if CAN_PLOT:
+            plt.figure()
+            plt.imshow( self.img2d, **kwargs)
+            plt.show()
+        else:
+            print("Cannot plot")
+            return None
+        #data = self.get_psana_data(index)* self.cspad_mask
+        #assemble_cspad.assemble_cspad(data, self.psf)
 
     def _set_pppg_args(self):
         """
@@ -104,12 +127,9 @@ class FormatD9114(FormatXTCCspad):
 
     @staticmethod
     def understand(image_file):
-        params = FormatD9114.get_params(image_file)
+        params = FormatXTCD9114.get_params(image_file)
         return params.experiment == "cxid9114" and \
                params.d9114.common_mode_algo in ['default', 'pppg', 'unbonded']
-
-    def get_detector(self, index=None):
-        return self._detector(self, index)
 
     def get_psana_data( self, index):
         self.event = self._get_event(index)
@@ -134,35 +154,57 @@ class FormatD9114(FormatXTCCspad):
     def get_raw_data(self, index):
         """this is really corrected data..."""
         data = self.get_psana_data(index)
-        data2d = self.cspad.image(self.event, data)
-        assert( data.dtype==np.float64)
-        #cctbx_det = self.get_detector(index)
-        #self._raw_data = []
-        #for quad_count, quad in enumerate(cctbx_det.hierarchy()):
-        #    for sensor_count, sensor in enumerate(quad):
-        #        for asic_count, asic in enumerate(sensor):
-        #            fdim, sdim = asic.get_image_size()
-        #            asic_data = data[sensor_count + quad_count * 8, :,
-        #                        asic_count * fdim:(asic_count + 1) * fdim]  # 8 sensors per quad
-        #            self._raw_data.append(flex.double(np.array(asic_data)))
-        #return tuple(self._raw_data)
-        return flex.double( data2d*self.img2d_mask)
+        assert(data.dtype == np.float64)
+        cctbx_det = self.get_detector(index)
+        self._raw_data = []
+        for quad_count, quad in enumerate(cctbx_det.hierarchy()):
+            for sensor_count, sensor in enumerate(quad):
+                for asic_count, asic in enumerate(sensor):
+                    fdim, sdim = asic.get_image_size()
+                    asic_data = data[sensor_count + quad_count * 8, :,
+                                asic_count * fdim:(asic_count + 1) * fdim]  # 8 sensors per quad
+                    self._raw_data.append(flex.double(np.array(asic_data)))
 
-    def get_detector(self, index=None):
-        return self._get_detector()
+        return tuple(self._raw_data)
+        #data2d = self.cspad.image(self.event, data)
+        #return flex.double( data2d*self.img2d_mask)
 
-    def _get_detector(self):
-        pixel_size=0.10992
-        fast = matrix.col((1.0, 0.0, 0.0))
-        slow = matrix.col((0.0, -1.0, 0.0))
-        image_size = (self.img_sh[1], self.img_sh[0])
-        orig = matrix.col((-image_size[0]*pixel_size/2.,
-                           image_size[1]*pixel_size/2., self.detector_distance))
-        trusted_range = (-100, 2**16)
-        return DetectorFactory.make_detector("", fast, slow, orig,
-            (pixel_size, pixel_size), image_size, trusted_range)
+    # ==================================================
+    # Beam methods
+
+    def _beam(self):
+        return self._get_beam()
+
+    def _get_beam(self):
+        return BeamFactory.simple_directional((0, 0, 1), WAVELEN_LOW)
+
+    def get_beam(self, index=None):
+        """
+        process the FEE spec here
+        """
+        return self._get_beam()
+
+    # ==================================================
+    # Detector methods
+
+    #def _detector(self):
+    #    return self._get_detector()
+
+    #def get_detector(self, index=None):
+    #    return self._get_detector()
+
+    #def _get_detector(self):
+    #    pixel_size=0.10992
+    #    fast = matrix.col((1.0, 0.0, 0.0))
+    #    slow = matrix.col((0.0, -1.0, 0.0))
+    #    image_size = (self.img_sh[1], self.img_sh[0])
+    #    orig = matrix.col((-image_size[0]*pixel_size/2.,
+    #                       image_size[1]*pixel_size/2., self.detector_distance))
+    #    trusted_range = (-100, 2**16)
+    #    return DetectorFactory.make_detector("", fast, slow, orig,
+    #        (pixel_size, pixel_size), image_size, trusted_range)
 
 if __name__ == '__main__':
     import sys
     for arg in sys.argv[1:]:
-        print(FormatD9114.understand(arg))
+        print(FormatXTCD9114.understand(arg))
