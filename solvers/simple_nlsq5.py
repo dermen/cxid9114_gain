0d55e529edcf4299ab0b83d50a5f654ee7ed4048
dermen <dermen@lbl.gov>
2019-02-21 22:09:08 +0900

    updates, now working sparse decoupler

--- solvers/simple_nlsq5.py
+++ solvers/simple_nlsq5.py
@@ -10,17 +10,16 @@ import sys
 
 np.random.seed(hash("no problem is insoluble in all conceivable circumstances")&((1<<32)-1) )
 
-Nshot = 10000  # total number of shots, each will get unique gain value
-max_meas_per_shot = 80  # e.g. how many Bragg reflections per shot
+Nshot = 50000  # total number of shots, each will get unique gain value
+max_meas_per_shot = 70  # e.g. how many Bragg reflections per shot
 min_meas_per_shot = 10
 xmin = -.1   # Gaussian amplitudes centered at x=0, so we will sample near peak amplitude
 xmax = .1
-Namp = 1000
-gains = np.random.uniform(1,3,Nshot)
+Namp = 8000
+gains = np.random.uniform(1,3,Nshot)  # gains per shot
 AmpsA = np.random.randint( 10,50,Namp)  # channel A amplitude
-AmpsB = np.random.uniform(AmpsA*.8,AmpsA*1.2)  # channel B amplitude
+AmpsB = np.random.uniform(AmpsA*.8,AmpsA*1.2)  # channel B amplitude offset slightly from channel A (e.g anomalous)
 Ngain = Nshot
-
 
 Niter = 5
 method = "lsmr"
@@ -29,42 +28,49 @@ method = "lsmr"
 # for each shot, pick some amplitudes and a gain
 gdata = []
 adata = []
+fdata = []
 Nmeas_per = []
+Nmeas_per_shot = np.random.randint(min_meas_per_shot, max_meas_per_shot, Nshot)
 for i_shot in range( Nshot):
     if i_shot % 50 ==0:
         print "\rBuilding data %d / %d shots" % (i_shot+1, Nshot),
         sys.stdout.flush()  # no new line
 
-    Nmeas_this_shot = np.random.randint(min_meas_per_shot, max_meas_per_shot+1)
-
-    amps = np.random.choice( range(Namp), size=Nmeas_this_shot, replace=False)
+    amps = np.random.choice( range(Namp), size=Nmeas_per_shot[i_shot], replace=False)
     adata.append( amps)
 
     gain = np.random.choice(range(Ngain))
-    gdata.append( [gain] * Nmeas_this_shot)
+    gdata.append( [gain] * Nmeas_per_shot[i_shot])
 
-    Nmeas_per.append( Nmeas_this_shot)
+    # its crucial that each color channel get a unique per shot adjustment
+    # else the gradient for either channel will be identical and the minimizer gets
+    # wont be able to disentangle
+    fdata.append( [np.random.uniform(0.2,0.8)] * Nmeas_per_shot[i_shot])  # fraction of channel A or B
+
 print "\rBuilding data %d / %d shots" % (Nshot, Nshot)
 
-Nmeas = np.sum( Nmeas_per)  # total measurements
+print "Combining data"
+Nmeas = np.sum( Nmeas_per_shot)  # total measurements
 gdata = np.hstack( gdata)
 adata = np.hstack( adata)
+fdata = np.hstack(fdata)
 xdata = np.random.uniform(xmin,xmax,Nmeas)
 
 # sum the two channel amplitudes for each measurement
-amp_sums = np.array(zip(AmpsA[adata], AmpsB[adata])).sum(axis=1)
+amp_sums = np.array(zip(AmpsA[adata]*fdata, AmpsB[adata]*(1-fdata))).sum(axis=1)
 
 # this is the measured intensity for each overlapping amplitude measurement
+print "adding gaussian noise to measurements"
 ydata = np.random.normal( amp_sums*np.exp(-xdata**2)*gains[gdata],0.5)
 
 class TestSolver:
-    def __init__(self, guess, xvals, yvals, gvals, avals, Namp, Ngain):
+    def __init__(self, guess, xvals, yvals, gvals, avals, fvals, Namp, Ngain):
         self.PRM = np.array(guess, float)
         self.xvals = xvals
         self.yvals = yvals
         self.gvals = gvals
         self.avals = avals
-
+        self.fvals = fvals
         self.Namp = Namp
         self.Ngain = Ngain
 
@@ -82,8 +88,8 @@ class TestSolver:
         BIG_row = []
         BIG_col =  []
         BIG_data = []
-        for i_meas, (xdata,ydata, gdata, adata) in enumerate(
-                zip(self.xvals, self.yvals, self.gvals, self.avals)):
+        for i_meas, (xdata, ydata, gdata, adata, fdata) in enumerate(
+                zip(self.xvals, self.yvals, self.gvals, self.avals, self.fvals)):
 
             # get the parameters from the pre-structured parameters array
             # which in this case is [AmplitudesA, AmplitudesB, Gains]
@@ -92,12 +98,14 @@ class TestSolver:
             gain_guess = self.PRM[2*self.Namp + gdata]
 
             # residual between data and guess
-            self.Beta[i_meas] = ydata - (ampA_guess+ampB_guess) * gain_guess * self.exp_factor[i_meas]
+            fA = fdata
+            fB = 1-fdata
+            self.Beta[i_meas] = ydata - (ampA_guess*fA + ampB_guess*fB) * gain_guess * self.exp_factor[i_meas]
 
             # partial derivitives
-            dA = gain_guess * self.exp_factor[i_meas]
-            dB = gain_guess * self.exp_factor[i_meas]
-            dG = (ampA_guess + ampB_guess) * self.exp_factor[i_meas]
+            dA = gain_guess * fA*self.exp_factor[i_meas]
+            dB = gain_guess * fB*self.exp_factor[i_meas]
+            dG = (ampA_guess*fA + ampB_guess*fB) * self.exp_factor[i_meas]
 
             # store the data in coordinate format for making sparse array
             BIG_col.extend([adata, self.Namp+adata, 2*self.Namp + gdata])
@@ -127,10 +135,10 @@ AmpB_guess = np.random.uniform(AmpsB*.1, AmpsB*3)
 Gain_guess = np.random.uniform(1, 2, Ngain)  # going in blind here on the gain
 PRM = np.hstack((AmpA_guess, AmpB_guess, Gain_guess))
 
-init_amp_sums = np.array(zip(AmpA_guess[adata], AmpB_guess[adata])).sum(axis=1)
+init_amp_sums = np.array(zip(AmpA_guess[adata]*fdata, AmpB_guess[adata]*(1-fdata))).sum(axis=1)
 init_yfit = init_amp_sums*np.exp(-xdata**2) * Gain_guess[gdata]
 
-solver = TestSolver(PRM, xdata, ydata, gdata, adata, Namp, Ngain)
+solver = TestSolver(PRM, xdata, ydata, gdata, adata,fdata, Namp, Ngain)
 import time
 tstart = time.time()
 for i in range(Niter):
@@ -140,7 +148,7 @@ time_solve = time.time() - tstart
 AmpA_final = solver.PRM[:Namp]
 AmpB_final = solver.PRM[Namp:2*Namp]
 Gain_final = solver.PRM[2*Namp:]
-final_amp_sums = np.array(zip(AmpA_final[adata], AmpB_final[adata])).sum(axis=1)
+final_amp_sums = np.array(zip(AmpA_final[adata]*fdata, AmpB_final[adata]*(1-fdata))).sum(axis=1)
 final_yfit = final_amp_sums*np.exp(-xdata**2)*Gain_final[gdata]
 
 print "\n\n++++++++++++++"
@@ -163,7 +171,7 @@ fig,axs = plt.subplots(1,3, figsize=(10,4)) #plt.figure()
 plt.suptitle("Fit analysis; %d Amplitudes (x2 channels) and %d gains (shots); %d measurements\n Sparsity=%.5g (%.2g %% occupancy in Jacobian); %d iters; time to solve: %.2f sec" \
              % (Namp, Ngain, Nmeas, sparsity, density*100, solver.niters, time_solve))
 
-axs[0].set_title("$(A_A + A_B) * G $")
+axs[0].set_title("$(A_if_i + A_i(1-f_i)) G_i $")
 axs[0].plot(amp_sums*gains[gdata],
             init_amp_sums*Gain_guess[gdata],'.', ms=.5)
 axs[0].plot(amp_sums*gains[gdata],
@@ -173,16 +181,16 @@ axs[0].set_ylabel("fit")
 axs[0].legend(("init guess", "final fit"), markerscale=10)
 
 
-axs[1].set_title("$(A_A)$")
+axs[1].set_title("$A_i$")
 axs[1].plot(AmpsA, AmpA_guess,'s', ms=4)
-axs[1].plot(AmpsA, AmpA_final,'o', ms=4)
+axs[1].plot(AmpsA, AmpA_final,'o', ms=3)
 axs[1].set_xlabel("data")
 axs[1].set_ylabel("fit")
 axs[1].legend(("init guess", "final fit"))
 
-axs[2].set_title("$(A_B)$")
+axs[2].set_title("$B_i$")
 axs[2].plot(AmpsB, AmpB_guess,'s', ms=4)
-axs[2].plot(AmpsB, AmpB_final,'o', ms=4)
+axs[2].plot(AmpsB, AmpB_final,'o', ms=3)
 axs[2].set_xlabel("data")
 axs[2].set_ylabel("fit")
 axs[2].legend(("init guess", "final fit"))
