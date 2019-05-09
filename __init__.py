77c4e44841ceaf960446c341c721c5bf31a6d0e3
Derek Mendez <dermen@lbl.gov>
2019-05-08 18:00:36 -0700

    in case solvers isnt build

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 from __future__ import division
-import boost.python
-from scitbx.lstbx import normal_eqns
-from scitbx_examples_bevington_ext import *
-ext = boost.python.import_ext("cxid9114_solvers_ext")
-from cxid9114_solvers_ext import *
+try:
+    import boost.python
+    from scitbx.lstbx import normal_eqns
+    from scitbx_examples_bevington_ext import *
+    ext = boost.python.import_ext("cxid9114_solvers_ext")
+    from cxid9114_solvers_ext import *
 
-
+except ImportError:
+    pass
 
