ddc2588dbf0e8c299c5fdca25e508adc98b7df01
dermen <dermen@lbl.gov>
2019-07-04 00:51:48 -0700

    karl solver plugins compiled finally

---
+++ solvers/karl_solver.py
@@ -0,0 +1,136 @@
+from argparse import ArgumentParser
+
+parser = ArgumentParser("eigen solvers")
+parser.add_argument("-i", type=str, help='input_file')
+parser.add_argument("-o", type=str, help='output mtz')
+parser.add_argument("-sim", action='store_true')
+parser.add_argument("-weights", action='store_true')
+parser.add_argument("-N", type=int, default=None, help="Nshots max")
+args = parser.parse_args()
+
+
+import numpy as np
+from cxid9114.solvers import solvers
+import cxid9114
+from scitbx.lstbx import normal_eqns_solving
+from scitbx.array_family import flex
+from scitbx.lstbx import normal_eqns
+from scitbx.examples.bevington.silver import levenberg_common
+
+
+class eigen_helper(cxid9114.log_sparse_jac_base,levenberg_common,normal_eqns.non_linear_ls_mixin):
+
+    def __init__(self, initial_estimates):
+
+        super(eigen_helper, self).__init__(n_parameters=len(initial_estimates))
+        self.initialize(initial_estimates)
+        self.stored_functional = []
+
+    def build_up(self, objective_only=False):
+        if not objective_only:
+            self.counter+=1
+
+        self.reset()
+        if not objective_only:
+            functional = self.functional_karl(self.x)   # NOTE: Im a cpp function in solvers_ext.cpp
+            print("\n\t<><><><><><>")
+            print("Functional value: %.10e" % functional)
+            print("\t<><><><><><>\n")
+            self.stored_functional.append(functional)
+
+        self.karlize(objective_only, current_values = self.x)
+
+
+class eigen_solver(solvers.LBFGSsolver):
+
+    def __init__(self, data, guess, truth=None, conj_grad=True, weights=None):
+        #solvers.LBFGSsolver.__init__(self, *args, **kwargs)  # NOTE: do it with lbfgs=False
+        # ^ brings in Yobs, LA, LB, PA, PB, Nhkl, Ns, Nmeas,   Aidx, Gidx
+        self.data = data
+        self.guess = guess
+        self.truth = truth
+
+        self._prepare_constants()
+
+        self._store_initial_guess()
+
+        self.counter = 0
+
+        # set dummie weights for now
+        if weights is None:
+            self.Wobs = flex.double(np.ones(len(self.Yobs)))
+        else:
+            self.Wobs = weights
+
+        self.helper = eigen_helper(initial_estimates=self.x_init) #, Nhkl=self.Nhkl)
+        self.helper.eigen_wrapper.conj_grad = conj_grad
+
+        # NOTE: I'm a cpp function defined in solvers_ext.cpp
+        self.helper.set_karl_data(self.Yobs, self.Wobs,
+            self.Aidx, self.Gidx,
+                self.PA, self.PB, self.LA, self.LB,
+            self.a_enA, self.a_enB, self.b_enA, self.b_enB, self.c_enA, self.c_enB,
+                self.Nhkl, self.Ns, )
+
+        self.helper.restart()
+
+        try:
+            _ = normal_eqns_solving.levenberg_marquardt_iterations_encapsulated_eqns(
+                   non_linear_ls=self.helper,
+                   n_max_iterations=200,
+                   track_all=True,
+                   step_threshold=0.0001)
+        except KeyboardInterrupt:
+            pass
+        print "End of minimization: Converged", self.helper.counter, "cycles"
+        print self.helper.get_eigen_summary()
+        print "Converged functional: ", self.helper.functional(self.helper.x)
+
+    def _prepare_constants(self):
+        """
+        LA,LB,PA,PB, a_lambda, b_lambda, c_lambda are all constants in this experiment
+        """
+        # TODO compute the a constants on the fly ?
+        self.LA = flex.double(np.ascontiguousarray(self.data["LA"]))
+        self.LB = flex.double(np.ascontiguousarray(self.data["LB"]))
+        self.LA = flex.double(np.ascontiguousarray(self.data["LA"]))
+        self.LB = flex.double(np.ascontiguousarray(self.data["LB"]))
+
+        self.a_enA = flex.double(np.ascontiguousarray(self.data["a_enA"]))
+        self.a_enB = flex.double(np.ascontiguousarray(self.data["a_enB"]))
+
+        self.b_enA = flex.double(np.ascontiguousarray(self.data["b_enA"]))
+        self.b_enB = flex.double(np.ascontiguousarray(self.data["b_enB"]))
+
+        self.c_enA = flex.double(np.ascontiguousarray(self.data["c_enA"]))
+        self.c_enB = flex.double(np.ascontiguousarray(self.data["c_enB"]))
+
+        self.Yobs = flex.double(np.ascontiguousarray(self.data["Yobs"]))
+
+    def _store_initial_guess(self):
+        """
+        the X parameter array will consist of a concatenation f 4 vectors
+        1 of length Nhkl for the |Fprotein|^2
+        1 of length Nhkl for the |Fheavy|^2
+        1 of length Nhkl for the alpha
+        1 of length Ns for the per-crystal scale factors
+        """
+        self.x_init = np.concatenate(
+            np.log(self.guess["Iprot_prm"]),
+            np.log(self.guess["Iheavy_prm"]),
+            self.guess["alpha_prm"],
+            self.guess["Gain_prm"])
+
+        # convert to flex
+        self.x_init = flex.double(np.ascontiguousarray(self.x_init))
+
+    def useful_scalars(self):
+        """
+        stores numbers of things
+        """
+        self.Nmeas = len(self.Yobs)
+        self.Nhkl = self.guess["Iprot_prm"].shape[0]
+        self.Ns = self.guess["Gain_prm"].shape[0]
+
+
+
--- solvers/solvers_ext.cpp
+++ solvers/solvers_ext.cpp
@@ -264,31 +264,174 @@ class log_sparse_jac_base: public scitbx::example::non_linear_ls_eigen_wrapper {
     // SET A SOLVE METHOD TO OVERRIDE THE CURRENT ONE
 
     void set_cpp_data(vecd y_obs_, vecd w_obs_,
-                    //vecd Gcurr_,
-                    //vecd IAcurr_,
-                    //vecd IBcurr_,
                     veci Aidx_,
                     veci Gidx_,
                     vecd PA_,
                     vecd PB_,
                     vecd LA_,
-                    vecd LB_, std::size_t Nhkl_,
+                    vecd LB_,
+                    std::size_t Nhkl_,
                     std::size_t Ns_){
         y_obs=y_obs_;
         w_obs=w_obs_;
         Aidx=Aidx_;Gidx=Gidx_;
-        //IAcurr = IAcurr_;
-        //IBcurr = IBcurr_;
-        //Gcurr = Gcurr_;
         LA=LA_;LB=LB_;PA=PA_;PB=PB_;
         Nhkl=Nhkl_;Ns=Ns_;
-
-        // initialize the jacobian sparse arrays
-        //for (int i=0; i < 3*y_obs.size(); i++){ // 3 parameters sampled  per measurement in this case..
-        //  jacobian_one_row_indices.push_back(0);
-        //  jacobian_one_row_data.push_back(0);
-        //  }
         }
+
+
+    void set_karl_data(vecd y_obs_, vecd w_obs_,
+                veci Aidx_,
+                veci Gidx_,
+                vecd PA_,
+                vecd PB_,
+                vecd LA_,
+                vecd LB_,
+                vecd EN_,
+                //vecd a_enA_,
+                //vecd a_enB_,
+                //vecd b_enA_,
+                //vecd b_enB_,
+                //vecd c_enA_,
+                //vecd c_enB_,
+                std::size_t Nhkl_,
+                std::size_t Ns_){
+        y_obs=y_obs_;
+        w_obs=w_obs_;
+        Aidx=Aidx_;Gidx=Gidx_;
+        LA=LA_;LB=LB_;PA=PA_;PB=PB_;
+        EN=EN_;
+        //a_enA=a_enA_;
+        //a_enB=a_enB_;
+
+        //b_enA=b_enA_;
+        //b_enB=b_enB_;
+
+        //c_enA=c_enA_;
+        //c_enB=c_enB_;
+        Nhkl=Nhkl_;Ns=Ns_;
+        }
+
+
+
+    vecd fvec_karl(vecd current_values) {
+          vecd y_diff = vecd(y_obs.size());
+          for (int i = 0; i < y_obs.size(); ++i){
+
+            std::size_t i_hkl = Aidx[i];
+            std::size_t i_s = Gidx[i];
+
+            double Iprot = std::exp(current_values[i_hkl]);
+            double Iheav = std::exp(current_values[Nhkl + i_hkl]);
+            double alpha = current_values[2*Nhkl + i_hkl];
+            double G = current_values[3*Nhkl + i_s];
+
+            double COS = std::cos(alpha);
+            double SIN = std::cos(alpha);
+
+            double Ipp = Iprot*Iprot;
+            double Ihh = Iheav*Iheav;
+            double IphCOS = Iprot*Iheav*COS;
+            double IphSIN = Iprot*Iheav*SIN;
+
+            double a_enA = EN[i_hkl];
+            double b_enA = EN[i_hkl + Nhkl];
+            double c_enA = EN[i_hkl + 2*Nhkl];
+            double a_enB = EN[i_hkl + 3*Nhkl];
+            double b_enB = EN[i_hkl + 4*Nhkl];
+            double c_enB = EN[i_hkl + 5*Nhkl];
+
+            double karl_A = Ipp + Ihh*a_enA + IphCOS*b_enA + IphSIN*c_enA;
+            double karl_B = Ipp + Ihh*a_enB + IphCOS*b_enB + IphSIN*c_enB;
+
+            double y_calc = G * (karl_A * LA[i]*PA[i]
+                                        + karl_B*LB[i]*PB[i]);
+            y_diff[i] = y_obs[i] - y_calc;
+          }
+          return y_diff;
+        }
+
+    void karlize(bool objective_only,
+                 scitbx::af::shared<double> current_values) {
+
+        vecd residuals = fvec_karl(current_values);
+        if (objective_only){
+          add_residuals(residuals.const_ref(), w_obs.const_ref());
+          return;
+        }
+
+        veci jacobian_one_row_indices(4);
+        vecd jacobian_one_row_data(4);
+
+        size_t* jacobian_one_row_indices_pt = &jacobian_one_row_indices[0];
+        double* jacobian_one_row_data_pt = &jacobian_one_row_data[0];
+
+        // add one of the normal equations per each observation
+        for (int ix = 0; ix < y_obs.size(); ++ix) {
+
+          std::size_t i_hkl = Aidx[ix];
+          std::size_t i_s = Gidx[ix];
+
+          double Iprot = std::exp(current_values[i_hkl]);
+          double Iheav = std::exp(current_values[Nhkl + i_hkl]);
+          double alpha = current_values[2*Nhkl + i_hkl];
+          double Gval = current_values[3*Nhkl + i_s];
+
+            double Ipp = Iprot*Iprot;
+            double Ihh = Iheav*Iheav;
+            double COS = std::cos(alpha);
+            double SIN = std::cos(alpha);
+            double IphCOS = Iprot*Iheav*COS;
+            double IphSIN = Iprot*Iheav*SIN;
+
+            double a_enA = EN[i_hkl];
+            double b_enA = EN[i_hkl + Nhkl];
+            double c_enA = EN[i_hkl + 2*Nhkl];
+            double a_enB = EN[i_hkl + 3*Nhkl];
+            double b_enB = EN[i_hkl + 4*Nhkl];
+            double c_enB = EN[i_hkl + 5*Nhkl];
+
+          double Aterm = LA[ix]*PA[ix];
+          double Bterm = LB[ix]*PB[ix];
+
+          // first derivitive of "yobs - ycalc" w.r.t. Iprot
+          double dIprot = Gval*Aterm*(2*Ipp + IphCOS*b_enA + IphSIN*c_enA)
+                + Gval*Bterm*(2*Ipp + IphCOS*b_enB + IphSIN*c_enB)   ;
+          jacobian_one_row_indices_pt[0]= i_hkl;
+          jacobian_one_row_data_pt[0] = dIprot;
+
+          // derivitive w.r.t. Iheav
+          double dIheav = Gval*Aterm*(2*Ihh*a_enA + IphCOS*b_enA + IphSIN*c_enA)
+                + Gval*Bterm*(2*Ihh*a_enB + IphCOS*b_enB + IphSIN*c_enB);
+          jacobian_one_row_indices_pt[1]= Nhkl + i_hkl;
+          jacobian_one_row_data_pt[1] = dIheav;
+
+          // derivitive w.r.t. alpha
+          double dalpha = Gval*Aterm*(-IphSIN*b_enA + IphCOS*c_enA)
+            +Gval*Bterm*(-IphSIN*b_enB + IphCOS*c_enB);
+          jacobian_one_row_indices_pt[2]= 2*Nhkl + i_hkl;
+          jacobian_one_row_data_pt[2] = dalpha;
+
+          // derivitive w.r.t. G
+          double dG = Aterm*(Ipp + Ihh*a_enA + IphCOS*b_enA + IphSIN*c_enA)
+            +Bterm*(Ipp + Ihh*a_enB + IphCOS*b_enB + IphSIN*c_enB);
+          jacobian_one_row_indices_pt[3]= 3*Nhkl + i_s;
+          jacobian_one_row_data_pt[3] = dG;
+
+          //add_equation(residuals[ix], jacobian_one_row.const_ref(), weights[ix]);
+          add_residual(-residuals[ix], 1.0);
+          add_equation_eigen(residuals[ix], jacobian_one_row_indices.const_ref(), jacobian_one_row_data.const_ref(), 1.);
+        }
+    }
+
+    double functional_karl(vecd current_values) {
+      double result = 0;
+      vecd fvec = fvec_karl(current_values);
+      for (int i = 0; i < fvec.size(); ++i) {
+        result += fvec[i]*fvec[i]*w_obs[i];
+      }
+      return result;
+    }
 
     vecd fvec_callable(vecd current_values) {
           vecd y_diff = vecd(y_obs.size());
@@ -306,7 +449,7 @@ class log_sparse_jac_base: public scitbx::example::non_linear_ls_eigen_wrapper {
             y_diff[i] = y_obs[i] - y_calc;
           }
           return y_diff;
-        }
+    }
 
     void access_cpp_build_up_directly_eigen_eqn(
                             bool objective_only,
@@ -363,7 +506,7 @@ class log_sparse_jac_base: public scitbx::example::non_linear_ls_eigen_wrapper {
           add_residual(-residuals[ix], 1.0);
           add_equation_eigen(residuals[ix], jacobian_one_row_indices.const_ref(), jacobian_one_row_data.const_ref(), 1.);
           }
-        }
+    }
 
     double functional(vecd current_values) {
       double result = 0;
@@ -375,13 +518,18 @@ class log_sparse_jac_base: public scitbx::example::non_linear_ls_eigen_wrapper {
     }
 
     vecd y_obs,w_obs,PA,PB,LA,LB;
+    //vecd a_enA, a_enB;
+    //vecd b_enA, b_enB;
+    //vecd c_enA, c_enB
+    vecd EN;
     veci Aidx,Gidx;
-    std::size_t Nhkl, n, Ns;
+    std::size_t Nhkl, n, Ns, Ne;
 
     //veci jacobian_one_row_indices(3);
     //vecd jacobian_one_row_data(3);
   };
 // END Sparse lev-mar tools
+
 
 
 namespace boost_python {
@@ -441,10 +589,12 @@ namespace boost_python {
     class_<lsjb,bases<nllsew> >( "log_sparse_jac_base", no_init)
 
       .def( init<int>(arg("n_parameters")))
+
       .def(
         "access_cpp_build_up_directly_eigen_eqn",
         &lsjb::access_cpp_build_up_directly_eigen_eqn,
         (arg("objective_only"),arg("current_values")))
+
       .def(
         "fvec_callable",
         &lsjb::fvec_callable,
@@ -460,10 +610,83 @@ namespace boost_python {
       .def(
         "set_cpp_data",
         &lsjb::set_cpp_data,
-        (arg("y_obs_"), arg("w_obs"), arg("Aidx_"), arg("Gidx_"), arg("PA_"),
+        (arg("y_obs_"), arg("w_obs_"), arg("Aidx_"), arg("Gidx_"), arg("PA_"),
          arg("PB_"), arg("LA_"), arg("LB_"), arg("Nhkl_"),
          arg("Ns_")) )
+
+      .def(
+        "functional_karl",
+        &lsjb::functional_karl,
+        (arg("current_values")) )
+
+      .def(
+        "fvec_karl",
+        &lsjb::fvec_karl,
+        (arg("current_values")) )
+
+
+      .def(
+        "karlize",
+        &lsjb::karlize,
+        (arg("objective_only"),arg("current_values")))
+
+      .def(
+        "set_karl_data",
+        &lsjb::set_karl_data,
+        (arg("y_obs_"),
+         arg("w_obs_"),
+         arg("Aidx_"),
+         arg("Gidx_"),
+         arg("PA_"),
+         arg("PB_"),
+         arg("LA_"),
+         arg("LB_"),
+         arg("EN_"),
+         arg("Nhkl_"),
+         arg("Ns_")) )
       ;
+
+
+
+      /*
+   class_<sparse_karl,bases<nllsew> >( "sparse_karl", no_init)
+
+      .def( init<int>(arg("n_parameters")))
+
+
+      .def(
+        "set_karl_data",
+        &sparse_karl::set_cpp_data,
+        (arg("y_obs_"), arg("w_obs_"), arg("Aidx_"), arg("Gidx_"), arg("PA_"),
+         arg("PB_"), arg("LA_"), arg("LB_"),
+           arg("a_enA_"),
+            arg("a_enB_"),
+            arg("b_enA_"),
+            arg("b_enB_"),
+            arg("c_enA_"),
+            arg("c_enB_"),
+            arg("Nhkl_"),
+            arg("Ns_")) )
+
+      .def(
+        "functional_karl",
+        &lsjb::functional_karl,
+        (arg("current_values")) )
+
+      .def(
+        "fvec_karl",
+        &lsjb::fvec_karl,
+        (arg("current_values")) )
+
+      .def(
+        "karlize",
+        &lsjb::karlize,
+        (arg("objective_only"),arg("current_values")))
+
+      ;
+      */
+
+
   }
 
 }}}
